31d3a05b98eb3810bc5ec56f551216c3d6f36c0c
Nick Sotula <so2niko@gmail.com>
2020-05-12 10:26:51 +0300

    docs: add info lesson 1

---
+++ les01/app/main.js
@@ -0,0 +1,66 @@
+import { name } from './second.js';
+
+console.log('It is worked', name);
+
+console.log(f);
+var f = 15;
+console.log(f);
+
+let a = 10;
+console.log('a', a);
+
+const b = 11;
+console.log('b', b);
+
+// b += 1;
+
+a = f; //КОпирование по значению
+f += 3;
+
+console.clear();
+console.log(f);
+console.log(a);
+
+2;
+'Hello'; "Hello your's";
+console.log(`Hello, ${f}`);
+true; false;
+undefined;
+null;
+
+let c = { val: 12};
+let d = { val: 13};
+
+d = c; //КОпирование по ссылке
+
+c.val = 66;
+
+console.log(c);
+console.log(d);
+
+//Symbol
+//BigInt
+
+//Операторы
+/*
++ - суммирование, конкатенация
+
+2 + 3 == 5 суммирование
+2 + '3' == '23' конкатенация
+'2' + 3 == '23' конкатенация
+
+
+
+-
+/
+*
+13 % 5 == 3 - остаток от деления
+2 ** 3 == 8 - возведение в степень
+
+*/
+
+let aa = '3';
+console.log(typeof []);//неявное приведение типов(к числу)
+
+let bb;
+
---
+++ les01/app/second.js
@@ -0,0 +1,2 @@
+export const name = 'Nick';
+console.log('I am a second');
